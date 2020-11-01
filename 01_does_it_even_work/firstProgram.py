2d6e6c28f91fc56300810975f57b3fd4bc7345d2
carlobiermann <carlo.biermann@protonmail.com>
2020-11-01 18:11:03 +0100

    First commit

---
+++ 01_does_it_even_work/firstProgram.py
@@ -0,0 +1,15 @@
+import time
+
+def printerfunc():
+    print("Hello, World!")
+    time.sleep(3)
+    print("Thanks for waiting")
+    return True
+
+printerfunc()
+
+if(printerfunc()):
+    print("Function returned true")
+
+print(printerfunc())
+
